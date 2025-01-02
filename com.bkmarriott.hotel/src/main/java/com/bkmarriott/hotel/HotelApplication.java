cd0a9aa9c6760a0af48034c80eecb0c502bde655
권현준 <bonheur9813@naver.com>
2025-01-03 04:42:13 +0900

    Refactor: 페이지네이션 관련 설정 WebConfig 클래스로 이동

--- com.bkmarriott.hotel/src/main/java/com/bkmarriott/hotel/HotelApplication.java
+++ com.bkmarriott.hotel/src/main/java/com/bkmarriott/hotel/HotelApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class HotelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
