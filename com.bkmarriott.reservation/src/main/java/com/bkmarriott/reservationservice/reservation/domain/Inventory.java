2635e370ef050a26d51191a5d81e415b7d10d8a8
DISNOTACAY <yeonjh915@gmail.com>
2025-01-03 02:32:53 +0900

    Feat: Add Inventory Domain Class

---
+++ com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/domain/Inventory.java
@@ -0,0 +1,37 @@
+package com.bkmarriott.reservationservice.reservation.domain;
+
+import com.bkmarriott.reservationservice.reservation.infrastructure.persistence.entity.RoomEntityType;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Inventory {
+
+  private InventoryId id;
+  private int totalInventory;
+  private int totalReserved;
+
+  @Builder
+  public Inventory(InventoryId id, int totalInventory, int totalReserved) {
+    this.id = id;
+    this.totalInventory = totalInventory;
+    this.totalReserved = totalReserved;
+  }
+
+  @Getter
+  public static class InventoryId {
+
+    private Long hotelId;
+    private LocalDate date;
+    private RoomEntityType roomType;
+
+    @Builder
+    public InventoryId(Long hotelId, LocalDate date, RoomEntityType roomType) {
+      this.hotelId = hotelId;
+      this.date = date;
+      this.roomType = roomType;
+    }
+  }
+
+}
