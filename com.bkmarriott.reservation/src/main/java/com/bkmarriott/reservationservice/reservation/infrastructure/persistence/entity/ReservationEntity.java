498b05b1c543b2c5f27e0e9269453be587c42698
DISNOTACAY <yeonjh915@gmail.com>
2025-01-03 17:27:18 +0900

    Refactor: 전역 예외 클래스 및 앤티티 이름 변경 사항

--- com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/infrastructure/persistence/entity/ReservationEntity.java
+++ com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/infrastructure/persistence/entity/ReservationEntity.java
@@ -9,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
-import java.util.Date;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -44,7 +43,7 @@ public class ReservationEntity extends BaseEntity {
 
   @Column(nullable = false)
   @Enumerated(EnumType.STRING)
-  private ReservationStatus status;
+  private ReservationEntityStatus status;
 
 
 }
