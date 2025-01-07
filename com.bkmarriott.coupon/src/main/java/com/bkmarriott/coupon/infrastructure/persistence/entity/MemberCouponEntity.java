2f5017b73d861d215609faefe008f1ea6c47eafd
zoohee <jjhh1545@gmail.com>
2025-01-07 10:50:12 +0900

    Modify: BaseEntity 적용 및 어노테이션 수정

--- com.bkmarriott.coupon/src/main/java/com/bkmarriott/coupon/infrastructure/persistence/entity/MemberCouponEntity.java
+++ com.bkmarriott.coupon/src/main/java/com/bkmarriott/coupon/infrastructure/persistence/entity/MemberCouponEntity.java
@@ -12,13 +12,14 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
+@NoArgsConstructor
 @Table(name = "m_member_coupon")
 @Entity(name = "MemberCoupon")
-public class MemberCouponEntity {
+public class MemberCouponEntity extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
