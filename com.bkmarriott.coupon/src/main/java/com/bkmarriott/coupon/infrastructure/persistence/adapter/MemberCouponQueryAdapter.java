7dd9a702b8f1a32ff6236e4b797e3e670010c87a
zoohee <jjhh1545@gmail.com>
2025-01-07 10:48:48 +0900

    Feat: CustomExceptionHandler 적용

--- com.bkmarriott.coupon/src/main/java/com/bkmarriott/coupon/infrastructure/persistence/adapter/MemberCouponQueryAdapter.java
+++ com.bkmarriott.coupon/src/main/java/com/bkmarriott/coupon/infrastructure/persistence/adapter/MemberCouponQueryAdapter.java
@@ -4,6 +4,7 @@ import com.bkmarriott.coupon.application.outputport.MemberCouponOutputPort;
 import com.bkmarriott.coupon.domain.MemberCoupon;
 import com.bkmarriott.coupon.infrastructure.persistence.entity.MemberCouponEntity;
 import com.bkmarriott.coupon.infrastructure.persistence.repository.MemberCouponRepository;
+import com.bkmarriott.coupon.presentation.rest.exception.MemberCouponNotFoundException;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,10 +17,9 @@ public class MemberCouponQueryAdapter implements MemberCouponOutputPort {
     private final MemberCouponRepository memberCouponRepository;
 
     public MemberCoupon getById(Long id) {
-        // TODO: CustomException 수정 필요
         return memberCouponRepository.findValidCouponById(id, LocalDateTime.now())
                 .map(MemberCouponEntity::toDomain)
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(MemberCouponNotFoundException::new);
     }
 
     @Transactional
