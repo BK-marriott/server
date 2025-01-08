fa69a954674072ac8b119d8d00a011e6f6349d67
권현준 <bonheur9813@naver.com>
2025-01-09 00:30:17 +0900

    Feat: 예약 기간에 따른 가용 객실 수 조회 기능 구현

--- com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/domain/Inventory.java
+++ com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/domain/Inventory.java
@@ -1,5 +1,6 @@
 package com.bkmarriott.reservationservice.reservation.domain;
 
+import com.bkmarriott.reservationservice.reservation.application.exception.NoAvailableRoomsException;
 import com.bkmarriott.reservationservice.reservation.domain.vo.RoomType;
 import java.time.LocalDate;
 import java.util.List;
@@ -45,5 +46,10 @@ public class Inventory {
   public static List<LocalDate> getDateRange(LocalDate startDate, LocalDate endDate) {
 
     return startDate.datesUntil(endDate).toList(); // endDate 제외
+  }
+
+  public int getAvailableRoomCount(){
+    if(totalInventory <= totalReserved) throw new NoAvailableRoomsException("남은 방이 없습니다.");
+    return totalInventory - totalReserved;
   }
 }
