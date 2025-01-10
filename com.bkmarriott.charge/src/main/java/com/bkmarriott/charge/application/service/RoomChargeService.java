f7afb57bd29484148df8d5fa21bab4cc5378113a
yjm07 <jyeon9506@gmail.com>
2025-01-10 14:50:25 +0900

    Feat: RoomChargeService with test

--- com.bkmarriott.charge/src/main/java/com/bkmarriott/charge/application/service/RoomChargeService.java
+++ com.bkmarriott.charge/src/main/java/com/bkmarriott/charge/application/service/RoomChargeService.java
@@ -2,14 +2,23 @@ package com.bkmarriott.charge.application.service;
 
 import com.bkmarriott.charge.application.exception.RoomChargeDuplicatedException;
 import com.bkmarriott.charge.application.exception.RoomChargeNotFoundException;
+import com.bkmarriott.charge.application.outputport.HotelTypeOutputPort;
 import com.bkmarriott.charge.application.outputport.RoomChargeOutputPort;
+import com.bkmarriott.charge.domain.DefaultRoomCharge;
 import com.bkmarriott.charge.domain.RoomCharge;
 import com.bkmarriott.charge.domain.vo.RoomChargeForCreate;
 import com.bkmarriott.charge.domain.vo.RoomChargeId;
+import com.bkmarriott.charge.domain.vo.RoomType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,6 +26,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class RoomChargeService {
 
     private final RoomChargeOutputPort roomChargeOutputPort;
+    private final HotelTypeOutputPort hotelTypeOutputPort;
 
     @Transactional
     public RoomCharge create(RoomChargeForCreate roomChargeForCreate) {
@@ -44,5 +54,20 @@ public class RoomChargeService {
                 .orElseThrow(RoomChargeNotFoundException::new);
 
         return roomChargeOutputPort.updateCharge(roomCharge, roomChargeForCreate.charge());
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 0 4 * * *")
+    public void createAllNextDefaultCharge() {
+        Map<RoomType, Integer> roomChargeMap = roomChargeOutputPort.findAllDefault().stream()
+                .collect(Collectors.toMap(DefaultRoomCharge::getRoomType, DefaultRoomCharge::getCharge));
+
+        LocalDate date = LocalDate.now().plusMonths(3);
+
+        List<RoomChargeForCreate> roomChargeForCreateList = hotelTypeOutputPort.findAll().stream()
+                .map(hotelType -> RoomChargeForCreate.of(hotelType.getHotelId(), hotelType.getRoomType(), date, roomChargeMap.get(hotelType.getRoomType())))
+                .toList();
+
+        roomChargeOutputPort.bulkCreate(roomChargeForCreateList);
     }
 }
