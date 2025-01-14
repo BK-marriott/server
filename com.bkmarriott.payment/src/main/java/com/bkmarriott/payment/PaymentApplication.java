096b07c1938d08127c6f9ddef262d24c209eb6d6
zoohee <jjhh1545@gmail.com>
2025-01-15 06:27:13 +0900

    Deploy All Service

--- com.bkmarriott.payment/src/main/java/com/bkmarriott/payment/PaymentApplication.java
+++ com.bkmarriott.payment/src/main/java/com/bkmarriott/payment/PaymentApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class PaymentApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
 	}
--- com.bkmarriott.promotion/src/main/java/com/bkmarriott/promotion/PromotionApplication.java
+++ com.bkmarriott.promotion/src/main/java/com/bkmarriott/promotion/PromotionApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PromotionApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PromotionApplication.class, args);
 	}
