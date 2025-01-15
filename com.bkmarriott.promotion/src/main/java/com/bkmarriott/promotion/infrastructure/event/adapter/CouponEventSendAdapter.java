a720f1ebdaba54e13aa65f924c995668950e5b33
hyunsb <junghss0113@gmail.com>
2025-01-15 12:23:34 +0900

    Modify: 사용되지 않는 메소드 삭제 및 카프카 이벤트 발행 클래스 분리

--- com.bkmarriott.promotion/src/main/java/com/bkmarriott/promotion/infrastructure/event/adapter/CouponEventSendAdapter.java
+++ com.bkmarriott.promotion/src/main/java/com/bkmarriott/promotion/infrastructure/event/adapter/CouponEventSendAdapter.java
@@ -4,11 +4,6 @@ import com.bkmarriott.promotion.application.outputport.CouponExternalMessageSend
 import com.bkmarriott.promotion.domain.event.CouponIssuanceEvent;
 import com.bkmarriott.promotion.domain.event.DomainEventEnvelop;
 import com.bkmarriott.promotion.infrastructure.event.publisher.CouponIssuanceEventPublisher;
-import com.bkmarriott.promotion.infrastructure.persistence.entity.CouponIssuanceOutboxEntity;
-import com.bkmarriott.promotion.infrastructure.persistence.repository.CouponIssuanceOutboxRepository;
-import com.bkmarriott.promotion.infrastructure.persistence.util.EventConverter;
-import jakarta.transaction.Transactional;
-import jakarta.transaction.Transactional.TxType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -19,21 +14,6 @@ import org.springframework.stereotype.Component;
 public class CouponEventSendAdapter implements CouponExternalMessageSender {
 
     private final CouponIssuanceEventPublisher eventPublisher;
-    private final CouponIssuanceOutboxRepository couponIssuanceOutboxRepository;
-    private final EventConverter eventConverter;
-
-    @Transactional
-    public void send(CouponIssuanceOutboxEntity entity) {
-        entity = couponIssuanceOutboxRepository.findById(entity.getOutboxId()).orElseThrow(() ->
-            new IllegalArgumentException("Outbox not found"));
-
-        if (!entity.isPublished()) {
-            DomainEventEnvelop<CouponIssuanceEvent> envelop = eventConverter.parseToEnvelopFrom(
-                entity);
-            eventPublisher.publish(envelop);
-            entity.toPublished();
-        }
-    }
 
     @Override
     public DomainEventEnvelop<CouponIssuanceEvent> sendMessage(DomainEventEnvelop<CouponIssuanceEvent> message) {
