25d88cc1f6371a1c2e9e44add3b5830e2c2f4772
hyunsb <junghss0113@gmail.com>
2025-01-17 14:51:34 +0900

    Test: 테스트 코드 구현

--- com.bkmarriott.coupon/src/test/java/com/bkmarriott/coupon/persistence/adapter/UserCouponAdapterTest.java
+++ com.bkmarriott.coupon/src/test/java/com/bkmarriott/coupon/persistence/adapter/UserCouponAdapterTest.java
@@ -1,20 +1,19 @@
 package com.bkmarriott.coupon.persistence.adapter;
 
 import com.bkmarriott.coupon.domain.Coupon;
-import com.bkmarriott.coupon.domain.CouponPolicy;
 import com.bkmarriott.coupon.domain.UserCoupon;
-import com.bkmarriott.coupon.domain.vo.CouponPolicyType;
 import com.bkmarriott.coupon.infrastructure.persistence.adapter.UserCouponCommandPersistenceAdapter;
 import com.bkmarriott.coupon.infrastructure.persistence.adapter.UserCouponQueryAdapter;
 import com.bkmarriott.coupon.infrastructure.persistence.entity.CouponEntity;
 import com.bkmarriott.coupon.infrastructure.persistence.entity.CouponPolicyEntity;
-import com.bkmarriott.coupon.infrastructure.persistence.exception.CouponNotSpentException;
+import com.bkmarriott.coupon.infrastructure.persistence.entity.CouponPolicyEntityType;
 import com.bkmarriott.coupon.infrastructure.persistence.exception.UserCouponNotFoundException;
 import com.bkmarriott.coupon.infrastructure.persistence.repository.CouponPolicyRepository;
 import com.bkmarriott.coupon.infrastructure.persistence.repository.CouponRepository;
 import com.bkmarriott.coupon.persistence.config.RepositoryTest;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +35,12 @@ class UserCouponAdapterTest {
 
     @Autowired
     private CouponRepository couponRepository;
+
+    @BeforeEach
+    void setUp() {
+        couponRepository.deleteAll();
+        couponPolicyRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("[성공] 유효한 쿠폰 조회 테스트 - 쿠폰 아이디로 기간이 유효하고 미사용인 쿠폰이 있으면 반환")
@@ -82,28 +87,20 @@ class UserCouponAdapterTest {
     }
 
     private Coupon generateTestCoupon() {
-        Coupon coupon = new Coupon(
-                null,
-                generateTestCouponPolicyFixedType(),
-                "Test Coupon",
-                0.1f
-        );
+        CouponEntity couponEntity = new CouponEntity(null, generateTestCouponPolicyFixedType(),
+            "test coupon", 10f);
 
-        CouponEntity couponEntity = couponRepository.save(CouponEntity.from(coupon));
+        couponEntity = couponRepository.save(couponEntity);
         return couponEntity.toDomain();
     }
 
-    private CouponPolicy generateTestCouponPolicyFixedType() {
-        CouponPolicy couponPolicy = new CouponPolicy(
-                null,
-                CouponPolicyType.FIXED,
-                null,
-                LocalDateTime.of(2025, 1, 3, 0, 0, 0),
-                LocalDateTime.of(2025, 1, 31, 0, 0, 0)
-                );
+    private CouponPolicyEntity generateTestCouponPolicyFixedType() {
+        CouponPolicyEntity couponPolicyEntity = new CouponPolicyEntity(null,
+            CouponPolicyEntityType.FIXED, null,
+            LocalDateTime.of(2025, 1, 3, 0, 0, 0),
+            LocalDateTime.of(2025, 1, 31, 0, 0, 0));
 
-        CouponPolicyEntity couponPolicyEntity = couponPolicyRepository.save(CouponPolicyEntity.from(couponPolicy));
-        return couponPolicyEntity.toDomain();
+        return couponPolicyRepository.save(couponPolicyEntity);
     }
 
     @Test
